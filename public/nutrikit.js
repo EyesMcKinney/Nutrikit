97a87282315ea05e43197460fb8c23ae7e0b2e6a
ism1762 <ism1762@rit.edu>
2022-03-26 10:37:10 -0400

    first commit

---
+++ public/nutrikit.js
@@ -0,0 +1,5 @@
+const proteins = [[steak, 300],[beef, 200],[chicken, 100],[fish, 80],[soy, 50]];
+const fruits = [[orange, 300],[banana, 200],[pineapple, 100], [grapes, 80],[blueberries, 50]];
+const vegtables = [[romaine, 30],[beans, 40],[squash, 100],[spinach, 50],[kale, 10]];
+const dairy = [[milk,  300],[yoghurt, 200],[cheddar, 200],[skim, 100],[cottage, 80]];
+const grains = [[bread, 200],[bagel, 300],[pita, 250],[naan, 210],[tortilla, 120]];
